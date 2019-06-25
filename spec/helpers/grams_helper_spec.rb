66b87c30a88f36e0fd00bf92e163dab06808fb28
Samantha Lee <samanthal.lee3@gmail.com>
2019-06-25 13:25:20 +0000

    Initial commit

---
+++ spec/helpers/grams_helper_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+RSpec.describe GramsHelper, type: :helper do
+
+# Specs in this file have access to a helper object that includes
+# the GramsHelper. For example:
+#
+# describe GramsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+end
