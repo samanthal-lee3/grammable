1e2b07186fec308638af749dfc62a18a0277d11f
Samantha Lee <samanthal.lee3@gmail.com>
2019-06-30 20:49:29 +0000

    Building create action in grams controller

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -6,5 +6,10 @@ class GramsController < ApplicationController
   def new
     @gram = Gram.new
   end
+
+  def create
+    @gram = gram.create(gram_params)
+    redirect_to root_path
+  end
   
 end
