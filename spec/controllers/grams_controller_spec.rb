fedaa5c260c90a8ba168badaca08159841da58c4
Samantha Lee <samanthal.lee3@gmail.com>
2019-07-02 19:54:35 +0000

    Passed grams#new and grams#index tests

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,36 +1,36 @@
 require 'rails_helper'
 
- RSpec.describe GramsController, type: :controller do
-#    describe "grams#index action" do
-#        it "should successfully show the page" do
-#          get :index
-#          expect(response).to have_http_status(:success)
-#        end
-#    end
+  RSpec.describe GramsController, type: :controller do
+     describe "grams#index action" do
+         it "should successfully show the page" do
+           get :index
+           expect(response).to have_http_status(:success)
+         end
+     end
 
   describe "grams#new action" do
-      # it "should successfully show the new form" do
-      #       user = FactoryBot.create(:user)
-      #       sign_in user
+       # it "should successfully show the new form" do
+       #       user = FactoryBot.create(:user)
+       #       sign_in user
 
       #       get :new
       #       expect(response).to have_http_status(:success)
       #     end
-      it "should successfully show the new form" do
-        get :new
-        expect(response).to have_http_status(:success)
+       it "should successfully show the new form" do
+         get :new
+        expect(response).to redirect_to new_user_session_path
+       end
+     end
+
+    describe "grams#create action" do
+          it "should successfully create a new gram in our database" do
+            post :create, params: {gram: {message: 'Hello!'}}
+            expect(response).to redirect_to user_session_path
+            
+            gram = Gram.last
+            expect(gram.message).to eq("Hello!")
+            #expect(gram.user).to eq(user)
+          end
       end
-    end
-
-    #describe "grams#create action" do
-        #it "should successfully create a new gram in our database" do
-          #post :create, params: {gram: {message: 'Hello!'}}
-          #expect(response).to redirect_to root_path
-
-          #gram = Gram.last
-          #expect(gram.message).to eq("Hello!")
-          #expect(gram.user).to eq(user)
-        #end
-    #end
 
 end
