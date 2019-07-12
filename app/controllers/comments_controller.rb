4a9d023da2876a84cbd95412c2f610fde4612f9c
Samantha Lee <samanthal.lee3@gmail.com>
2019-07-12 02:17:01 +0000

    Styling index page for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
+
   def create
     @gram = Gram.find_by_id(params[:gram_id])
     return render_not_found if @gram.blank?
