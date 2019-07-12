df55a59af376e3a5cfcce4a59224b597e47fb453
Samantha Lee <samanthal.lee3@gmail.com>
2019-07-12 02:41:07 +0000

    Working comments form on index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "grams#index"
-  resources :grams, only: [:new, :create, :show, :edit, :update, :destroy]
-  resources :comments, only: :create
+  resources :grams do
+    resources :comments, only: :create
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
 end
 
